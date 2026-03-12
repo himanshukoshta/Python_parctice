24f556f62a86e72abe2410bda1e713d67d8e340b
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-12 16:07:06 +0530

    In-place symmetric difference update

---
+++ symmetric_diff.py
@@ -0,0 +1,5 @@
+## Create two sets and update the first set with the symmetric difference of the two sets. Print the modified first set.
+set1={1,2,3,4,5,6}
+set2={4,5,6,7,8}
+set1.symmetric_difference_update(set2)
+print(set1)
