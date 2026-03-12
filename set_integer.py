f0a73256ba6ad3147875ab77e02d9be36a323b7b
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-12 15:59:27 +0530

    Create and print set of first 10 integers

---
+++ set_integer.py
@@ -0,0 +1,10 @@
+## Create a set with the first 10 positive integers. Print the set.
+s1={1,2,3,4,5,6,7,8,9,10}
+print(s1)
+# adding 11 in the set
+s1.add(11)
+print(s1)
+
+# removing 1 from set
+a=s1.remove(1)
+print(s1)
