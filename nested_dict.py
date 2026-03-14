ec106c183b9814606630af7abba631154143d91c
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-14 21:17:31 +0530

    Nested Dictionaries

---
+++ nested_dict.py
@@ -0,0 +1,12 @@
+## Create a nested dictionary representing a student with keys 'name', 'age', 'grades', where 'grades' is another dictionary with
+#  keys 'math', 'science', and 'english'. Print the nested dictionary.
+student={
+    "name":"himanshu",
+    "age":20,
+    "grade":{
+        "maths":70,
+        "chemistry":65,
+        "physics":62
+    }
+}
+print(student)
