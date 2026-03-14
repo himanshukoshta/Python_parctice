ef1b7d5e14dcf2febfbc40962591ee60b00e4c93
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-14 21:16:37 +0530

    Dictionary Comprehensions

---
+++ comprehension_dict.py
@@ -0,0 +1,4 @@
+## Create a new dictionary containing the cubes of the first 10 positive integers using a dictionary comprehension.
+#  Print the new dictionary.
+cubes={i:i**3 for i in range(1,11)}
+print(cubes)
