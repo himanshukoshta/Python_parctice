db0d7a5913f7f51a8a121097959ccd68834b2e2c
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-14 21:15:08 +0530

    Accessing Dictionary Elements

---
+++ accessing_elements.py
@@ -0,0 +1,4 @@
+## Print the value of the key 5 and the keys of the dictionary created in Assignment 1.
+d1={i:i**2 for i in range(1,11)}
+print(f"value of 5: {d1[5]}")
+print(f"keys {list(d1.keys())}")
