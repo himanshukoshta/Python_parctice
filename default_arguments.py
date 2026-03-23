01cf60f42b44d622548528ea5ad3c245c7f84b0c
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-23 12:39:57 +0530

    add add function with default parameter value

---
+++ default_arguments.py
@@ -0,0 +1,7 @@
+## Define a function that takes two integers as input and returns their sum. The second integer should have a default value of 5.
+#  Test the function with different inputs.
+def sum(x,y=5):
+    return x+y
+
+print(sum(8))
+print(sum(10))
