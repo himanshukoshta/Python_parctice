5b7c66e2a5592c788d29b846d0d8f912e056a770
Himanshu Koshta <koshtahimanshu89@gmail.com>
2026-03-23 12:46:43 +0530

    implement person string formatter using named arguments

---
+++ keyword_argument.py
@@ -0,0 +1,7 @@
+## Define a function that takes three named arguments: first_name, last_name, and age, and returns a formatted string. 
+# Test the function with different inputs.
+def format_name(first_name, last_name,age):
+    return f"{first_name} {last_name} is {age} years old"
+
+print(format_name('himanshu','koshta',20))
+print(format_name('Arjun','Singh',21))
